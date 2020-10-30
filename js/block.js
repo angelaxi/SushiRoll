b310bdb7d54c665bd26cefbb7fe8b8be2dec8e76
Angela Xi <angelaxi@Angelas-MacBook-Pro.local>
2020-10-30 00:21:18 -0400

    controls are working

--- js/block.js
+++ js/block.js
@@ -1,38 +1,198 @@
+var sushi
+
 class Block {
-    constructor(scene, currPos) {
-        this.scene = scene;
-        this.currPos = currPos;
-        this.geometry = new THREE.BoxGeometry(1, 2, 1);
-        this.material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        this.block = new THREE.Mesh( this.geometry, this.material );
-        // this.block.rotation.set(2, 0, 0.4);
-        // this.geometry.translate(-2, 0, -0.4);
-        // this.geometry.translate(this.currPos[0].x, this.currPos[0].y, this.currPos[0].z);
-        this.bbox = new THREE.Box3().setFromObject(this.block);
-        this.block.position.set(this.bbox.min.x, this.bbox.max.y, 0);
-        this.pivot = new THREE.Group();
-        this.pivot.add(this.block);
-        // this.pivot.position.set(-this.bbox.min.x, 0.5 - this.bbox.max.y, 0.5);
-        this.scene.add(this.pivot);
+    constructor(scene, currPos, orientation) {
+        this.orientation = orientation
+        this.scene = scene
+        this.currPos = currPos
+        // this.geometry = new THREE.BoxGeometry(1, 2, 1)
+        this.geometry = createBoxWithRoundedEdges(1, 2, 1, .25, 3)
+        this.geometry.computeVertexNormals()
+
+        var loader = new THREE.CubeTextureLoader();
+        loader.setCrossOrigin( "" );
+        loader.setPath( 'https://threejs.org/examples/textures/cube/pisa/' );
+
+        var cubeTexture = loader.load( [
+        'px.png', 'nx.png',
+        'py.png', 'ny.png',
+        'pz.png', 'nz.png'
+        ] );
+        
+        this.material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: cubeTexture } );
+
+        // var textureMap = new THREE.CubeTextureLoader().load("textures/rice.jpg")
+        // this.material = new THREE.MeshBasicMaterial( { color: 0xffffff, map: textureMap } );
+
+        // this.material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
+        this.block = new THREE.Mesh( this.geometry, this.material )
+        // this.geometry.translate(currPos[0].x, currPos[0].y + 1, currPos[0].z)
+        sushi = this.block
+        // this.block.rotation.set(2, 0, 0.4)
+        // this.geometry.translate(-2, 0, -0.4)
+        // this.geometry.translate(this.currPos[0].x, this.currPos[0].y, this.currPos[0].z)
+        // this.bbox = new THREE.Box3().setFromObject(this.block)
+        // this.block.position.set(this.bbox.min.x, this.bbox.max.y, 0)
+        // this.pivot = new THREE.Object3D()
+        // this.pivot.add(this.block)
+        // this.pivot.position.set(-this.bbox.min.x, 0.5 - this.bbox.max.y, 0.5)
+        // this.scene.add(this.pivot)
+        this.scene.add(this.block)
+        console.log("block position")
         console.log(this.block.position)
-        console.log(this.pivot.position)
-        // console.log(this.bbox.min.x)
-        // console.log(this.bbox.max.y)
-        // console.log(-this.bbox.min.x)
-        // console.log(0.5 - this.bbox.max.y)
     }
 
+    // animate() {
+    //     requestAnimationFrame(animate)
+    //     this.block.rotation.z -= 0.1
+    //     renderer.render(scene, camera)
+    // }
+
     rotate(direction) {
-        // switch(direction) {
-        //     case "right":
+        var position = {x : this.block.position.x, y: this.block.position.y, z: this.block.position.z}
+        var rotation = {x : this.block.rotation.x, y: this.block.rotation.y, z: this.block.rotation.z}
+        var positionEnd = {x : this.block.position.x, y: this.block.position.y, z: this.block.position.z}
+        var rotationEnd = {x : this.block.rotation.x, y: this.block.rotation.y, z: this.block.rotation.z}
+        var scale = {x: 1, y: 1, z: 1}
+        var rot = 1.5708
 
-        // }
-        // this.block.position.set(this.bbox.min.x, this.bbox.max.y, 0);
-        // this.pivot.position.set(0, 0, 0.5);
-        // this.pivot.rotation.z -= 1.5708;
+        console.log(this.orientation)
 
-        this.pivot.position.set(-this.bbox.min.x, 0.5 - this.bbox.max.y, 0.5);
-        this.pivot.rotation.x += 1.5708;
-        // console.log(this.block.position)
+        if (direction == "right") {
+            rotationEnd.z -= rot
+            switch (this.orientation) {
+                case "vertical":
+                    positionEnd.x += 1.5
+                    positionEnd.y -= 0.5
+                    scale.x = 2
+                    scale.y = 0.5
+                    this.orientation = "horizontalX"
+                    break
+                case "horizontalX":
+                    positionEnd.x += 1.5
+                    positionEnd.y += 0.5
+                    this.orientation = "vertical"
+                    break
+                case "horizontalZ":
+                    positionEnd.x += 1.0
+                    scale.y = 0.5
+                    scale.z = 2
+                    this.orientation = "horizontalZ"
+                    break
+            }
+        } else if (direction == "left") {
+            rotationEnd.z += rot
+            switch (this.orientation) {
+                case "vertical":
+                    positionEnd.x -= 1.5
+                    positionEnd.y -= 0.5
+                    scale.x = 2
+                    scale.y = 0.5
+                    this.orientation = "horizontalX"
+                    break
+                case "horizontalX":
+                    positionEnd.x -= 1.5
+                    positionEnd.y += 0.5
+                    this.orientation = "vertical"
+                    break
+                case "horizontalZ":
+                    positionEnd.x -= 1.0
+                    scale.y = 0.5
+                    scale.z = 2
+                    this.orientation = "horizontalZ"
+                    break
+            }
+        } else if (direction == "forward") {
+            rotationEnd.x += rot
+            switch (this.orientation) {
+                case "vertical":
+                    positionEnd.z += 1.5
+                    positionEnd.y -= 0.5
+                    scale.z = 2
+                    scale.y = 0.5
+                    this.orientation = "horizontalZ"
+                    break
+                case "horizontalX":
+                    positionEnd.z += 1.0
+                    scale.x = 2
+                    scale.y = 0.5
+                    this.orientation = "horizontalX"
+                    break
+                case "horizontalZ":
+                    positionEnd.z += 1.5
+                    positionEnd.y += 0.5
+                    this.orientation = "vertical"
+                    break
+            }
+        } else if (direction == "backward") {
+            rotationEnd.x -= rot
+            switch (this.orientation) {
+                case "vertical":
+                    positionEnd.z -= 1.5
+                    positionEnd.y -= 0.5
+                    scale.z = 2
+                    scale.y = 0.5
+                    this.orientation = "horizontalZ"
+                    break
+                case "horizontalX":
+                    positionEnd.z -= 1.0
+                    scale.x = 2
+                    scale.y = 0.5
+                    this.orientation = "horizontalX"
+                    break
+                case "horizontalZ":
+                    positionEnd.z -= 1.5
+                    positionEnd.y += 0.5
+                    this.orientation = "vertical"
+                    break
+            }
+        }
+
+        console.log("target rotation")
+        console.log(rotationEnd)
+        console.log("scale")
+        console.log(scale)
+
+        var tweenPos = new TWEEN.Tween(position).to(positionEnd, 300);
+        tweenPos.easing(TWEEN.Easing.Quadratic.Out);
+        tweenPos.start();
+        tweenPos.onUpdate(function(){
+            sushi.position.set(position.x, position.y, position.z);
+        });
+
+        var tweenRot = new TWEEN.Tween(rotation).to(rotationEnd, 300);
+        tweenRot.easing(TWEEN.Easing.Quadratic.Out);
+        tweenRot.start();
+        tweenRot.onUpdate(function(){
+            sushi.rotation.set(rotation.x, rotation.y, rotation.z);
+        });
+        tweenRot.onComplete(function(){ 
+            // reset rotation
+            sushi.scale.set(scale.x, scale.y, scale.z)
+            sushi.rotation.set(0, 0, 0)
+        });
+    }
+
+    createBoxWithRoundedEdges( width, height, depth, radius0, smoothness ) {
+        let shape = new THREE.Shape();
+        let eps = 0.00001;
+        let radius = radius0 - eps;
+        shape.absarc( eps, eps, eps, -Math.PI / 2, -Math.PI, true );
+        shape.absarc( eps, height -  radius * 2, eps, Math.PI, Math.PI / 2, true );
+        shape.absarc( width - radius * 2, height -  radius * 2, eps, Math.PI / 2, 0, true );
+        shape.absarc( width - radius * 2, eps, eps, 0, -Math.PI / 2, true );
+        let geometry = new THREE.ExtrudeBufferGeometry( shape, {
+          depth: depth - radius0 * 2,
+          bevelEnabled: true,
+          bevelSegments: smoothness * 2,
+          steps: 1,
+          bevelSize: radius,
+          bevelThickness: radius0,
+          curveSegments: smoothness
+        });
+        
+        geometry.center();
+        
+        return geometry;
     }
 }
--- js/header.js
+++ js/header.js
@@ -1,71 +1,125 @@
-var scene;
-var camera;
-var camera_piv;
-var camera_y;
-var container;
-var header;
-var renderer;
-var pickingTexture;
-var mainmenu_sprite;
-var geometry;
-var cube;
-var pivot;
-var block;
+var scene
+var camera
+var camera_piv
+var camera_y
+var container
+var header
+var renderer
+var pickingTexture
+var mainmenu_sprite
+var geometry
+var cube
+var pivot
+var block
 
-init();
-animate();
+init()
+animate()
 
 function init() {
-  pickingTexture = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight );
-	pickingTexture.texture.minFilter = THREE.LinearFilter;
-  container = document.getElementById("TitleHeader");
-  canvas = document.createElement("canvas");
-  var WIDTH = container.offsetWidth;
-  var HEIGHT = container.offsetHeight;
+  pickingTexture = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight )
+	pickingTexture.texture.minFilter = THREE.LinearFilter
+  container = document.getElementById("TitleHeader")
+  canvas = document.createElement("canvas")
+  var WIDTH = container.offsetWidth
+  var HEIGHT = container.offsetHeight
 
-  renderer = new THREE.WebGLRenderer({antialias:true, canvas:canvas, alpha:true});
-  renderer.setSize(WIDTH, HEIGHT);
-  container.appendChild(renderer.domElement);
+  renderer = new THREE.WebGLRenderer({antialias:true, canvas:canvas, alpha:true})
+  renderer.setSize(WIDTH, HEIGHT)
+  container.appendChild(renderer.domElement)
 
-  // camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 20000);
-  // camera.position.set(0, 0, 10);
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 1000);
-  camera.position.set(4, 5, 10);
+  // camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 20000)
+  // camera.position.set(0, 0, 10)
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 1000)
+  camera.position.set(4, 5, 12)
   camera.lookAt(0, 0, 0)
 
-  scene = new THREE.Scene();
+  scene = new THREE.Scene()
 
-  var currPos = [new THREE.Vector3(-2, 0, 0)];
-  var board = new Board("vertical", currPos, scene);
-  board.createBoard(0)
+  var orbitControls = new THREE.OrbitControls(camera, canvas);
 
-  block = new Block(scene, currPos);
+  var light = new THREE.DirectionalLight(0xffffff, 1.5);
+  light.position.setScalar(10);
+  scene.add(light);
+  scene.add(new THREE.AmbientLight(0xffffff, 0.5));
 
-  // geometry = new THREE.BoxGeometry(1, 1, 2);
-  // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  // cube = new THREE.Mesh( geometry, material );
-  // cube.rotation.set(2, 0, 0.4)
-  // geometry.translate(-2, 0, -0.4);
+  var startOrient = "vertical"
+  var currPos = [new THREE.Vector3(0, 0, 1)]
+  var board = new Board(startOrient, currPos, scene, renderer)
+  board.createBoard(5)
 
-  // var bbox = new THREE.Box3().setFromObject(cube);
-  // cube.position.set(bbox.min.x, bbox.max.y, 0);
-  // pivot = new THREE.Group();
-  // pivot.add(cube);
-  // pivot.position.set(-bbox.min.x, 0.5-bbox.max.y, 0.5);
-  // scene.add(pivot);
+  block = new Block(scene, currPos, startOrient)
 
-  function render() {
-    requestAnimationFrame( render );
-    renderer.render( scene, camera );
-  }
+  // var roundedBoxGeometry = createBoxWithRoundedEdges(1, 1, 1, .25, 3);
+  // roundedBoxGeometry.computeVertexNormals();
+  // roundedBoxGeometry.translate(0, .5, 0);
+
+  // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
+  // cube = new THREE.Mesh( roundedBoxGeometry, material )
+  // scene.add(cube)
+
+  // var bbox = new THREE.Box3().setFromObject(cube)
+  // cube.position.set(bbox.min.x, bbox.max.y, 0)
+  // pivot = new THREE.Group()
+  // pivot.add(cube)
+  // pivot.position.set(-bbox.min.x, 0.5-bbox.max.y, 0.5)
+  // scene.add(pivot)
+
+  // var position = { x : cube.position.x, y: cube.position.y, z: cube.position.z};
+  // var end = { x : cube.position.x + 2.0, y: cube.position.y, z: cube.position.z};
+  // var tween = new TWEEN.Tween( position ).to( end, 200 );
+  // tween.easing(TWEEN.Easing.Quadratic.Out);
+  // tween.start();
+  // tween.onUpdate(function(){ 
+  //     console.log("position")
+  //     cube.position.set(position.x,position.y, position.z);
+  // });
+  // var rotation = { x : cube.rotation.x, y: cube.rotation.y, z: cube.rotation.z}
+  // var end2 = { x : cube.rotation.x, y: cube.rotation.y, z: cube.rotation.z - 1.5708}
+  // var tween2 = new TWEEN.Tween(rotation).to(end2, 200);
+  // tween2.easing(TWEEN.Easing.Quadratic.Out);
+  // tween2.start();
+  // tween2.onUpdate(function(){ 
+  //     console.log("rotation")
+  //     cube.rotation.set(rotation.x,rotation.y, rotation.z);
+  // });
    
-  render();
+  // render()
+}
+
+function createBoxWithRoundedEdges( width, height, depth, radius0, smoothness ) {
+  let shape = new THREE.Shape();
+  let eps = 0.00001;
+  let radius = radius0 - eps;
+  shape.absarc( eps, eps, eps, -Math.PI / 2, -Math.PI, true );
+  shape.absarc( eps, height -  radius * 2, eps, Math.PI, Math.PI / 2, true );
+  shape.absarc( width - radius * 2, height -  radius * 2, eps, Math.PI / 2, 0, true );
+  shape.absarc( width - radius * 2, eps, eps, 0, -Math.PI / 2, true );
+  let geometry = new THREE.ExtrudeBufferGeometry( shape, {
+    depth: depth - radius0 * 2,
+    bevelEnabled: true,
+    bevelSegments: smoothness * 2,
+    steps: 1,
+    bevelSize: radius,
+    bevelThickness: radius0,
+    curveSegments: smoothness
+  });
+  
+  geometry.center();
+  
+  return geometry;
+}
+
+function render() {
+  // requestAnimationFrame( render )
+  renderer.render( scene, camera )
 }
 
 function animate() {
-  requestAnimationFrame(animate);
+  requestAnimationFrame(animate)
+  TWEEN.update()
 
-  renderer.render(scene, camera);
+  // pivot.rotation.z += 0.1
+  renderer.render(scene, camera)
 }
 
 document.addEventListener('keydown', function(event) {
@@ -76,5 +130,14 @@ document.addEventListener('keydown', function(event) {
   if (event.key == 'ArrowRight') {
     console.log("right arrow pressed")
     block.rotate("right")
+  } else if (event.key == 'ArrowLeft') {
+    console.log("left arrow pressed")
+    block.rotate("left")
+  } else if (event.key == 'ArrowDown') {
+    console.log("down arrow pressed")
+    block.rotate("forward")
+  } else if (event.key == 'ArrowUp') {
+    console.log("up arrow pressed")
+    block.rotate("backward")
   }
-});
+})
